37c43cd89d1eb990fe4b6c210f59b00291619e48
James Shaun <SHAUNJ@bridgwater.ac.uk>
2023-08-09 13:16:59 +0100

    Added logic for autofill for Pokemon team selection

--- PGBattleSupport/src/components/SelectPokemon.tsx
+++ PGBattleSupport/src/components/SelectPokemon.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useAppContext } from "../Context";
+import { KeyboardEvent } from "react";
 
 import "../App.css";
 
@@ -11,13 +12,27 @@ function SelectPokemon() {
     margin: "auto",
   };
 
+  const FilterPokemon = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(evt);
+    let inputs = String(evt.currentTarget.value);
+    console.log(inputs.trim());
+    let matching = allPokemon?.filter((pokemon) => {
+      return pokemon.name.includes(inputs.toUpperCase());
+    });
+    console.log(matching);
+  };
+
   return (
     <div style={style} className="col">
       <h1>{allPokemon?.length}</h1>
 
       <label>Select Pokemon</label>
 
-      <input name="selectPokemon" id="selectPokemon" />
+      <input
+        name="selectPokemon"
+        id="selectPokemon"
+        onKeyUp={(evnt) => FilterPokemon(evnt)}
+      />
       <button className="btn" type="button">
         Add
       </button>
